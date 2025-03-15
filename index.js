81b977ca83f5a9b66cf065d816f877e779e491b8
Maxim Bogdanov <126229962+dtfyu3@users.noreply.github.com>
2025-03-15 18:03:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const tileserver = require('tileserver-gl');
-const path = require('path');
+import tileserver from 'tileserver-gl';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Получаем __dirname в ES-модулях
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Путь к конфигурационному файлу
 const configPath = path.join(__dirname, 'tileserver', 'config.json');
 
-
+// Запуск tileserver-gl
 const server = tileserver(configPath);
-module.exports = server;
+
+// Экспорт функции для Vercel
+export default server;
